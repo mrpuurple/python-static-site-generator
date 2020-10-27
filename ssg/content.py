9504e8e209abe7150b1d42fea4c603c7a0bca26f
Nichlas Handstedt <nichlas.handstedt@scania.com>
2020-10-27 18:17:33 +0100

    fis return in type property

--- ssg/content.py
+++ ssg/content.py
@@ -24,7 +24,7 @@ class Content(Mapping):
 
     @property
     def type(self):
-        self.data["type"] if "type" in self.data else None
+        return self.data["type"] if "type" in self.data else None
 
     @type.setter
     def type(self, type):
