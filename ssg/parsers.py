18480906f6c1c4e079f0dd7780ac35fa9dc9f76f
Nichlas Handstedt <nichlas.handstedt@scania.com>
2020-10-27 18:37:03 +0100

    add parse method

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -42,4 +42,9 @@ class MarkdownParser(Parser):
     def parse(self, path: Path, source: Path, dest: Path):
         content = Content.load(self.read(path))
         html = markdown(content.body)
-        
+        self.write(path, dest, html)
+        sys.stdout.write(
+            "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(
+                path.name, content
+                )
+            )
